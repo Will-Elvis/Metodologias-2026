530236f8fa78d043b168b70659d8bf9ee6aa326a
Huarachi Wilson <will.ehm.dev@gmail.com>
2026-03-28 19:16:47 -0300

    Se crea el proyecto base Practica01 correctamente

---
+++ Practica01/Practica01/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: PcHome
+ * Fecha: 28/3/2026
+ * Hora: 19:14
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Practica01
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
