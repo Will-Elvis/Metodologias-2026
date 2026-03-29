4e79581f6e3341d73ffeafcf671072ad6519b155
Huarachi Wilson <will.ehm.dev@gmail.com>
2026-03-29 18:40:19 -0300

    Ejercicio 2 : Creamos la clase Numero y Implementamos Comparable

---
+++ Practica01/Practica01/Numero.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: PcHome
+ * Fecha: 29/3/2026
+ * Hora: 18:07
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Practica01
+{
+	/// <summary>
+	/// Description of Numero.
+	/// </summary>
+	public class Numero : Comparable
+	{
+		private int valor;
+		
+		public Numero(int valor)
+		{
+			this.valor = valor;
+		}
+		
+		public int getValor(){
+			return this.valor;
+		}
+		
+		public bool sosIgual(Comparable c)
+		{
+			return this.valor == ((Numero)c).getValor();
+		}
+		public bool sosMenor(Comparable c)
+		{
+			return this.valor < ((Numero)c).getValor();
+		}
+		public bool sosMayor(Comparable c)
+		{
+			return this.valor > ((Numero)c).getValor();
+		}
+		
+	}
+}
