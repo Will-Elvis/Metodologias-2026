c00d6a68f66ccaa5b8ff9b0a0cf5c8ab30f28720
Huarachi Wilson <will.ehm.dev@gmail.com>
2026-03-30 17:32:35 -0300

    Ejercicio 3 : Implementamos la interfaz IColeccionable

---
+++ Practica01/Practica01/IColeccionable.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: PcHome
+ * Fecha: 30/3/2026
+ * Hora: 17:27
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Practica01
+{
+	/// <summary>
+	/// Description of IColeccionable.
+	/// </summary>
+	public interface IColeccionable
+	{
+		int cuantos();
+		int maximo();
+		int minimo();
+		void agregar(Comparable c);
+		bool contiene(Comparable c);
+	}
+}
