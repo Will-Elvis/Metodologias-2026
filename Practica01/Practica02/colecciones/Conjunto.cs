2472ea41c851c5730702b65e3d97141f9e34c513
Huarachi Wilson <will.ehm.dev@gmail.com>
2026-04-22 12:22:24 -0300

    Ejercicio 3 del TP2 Realizado: Se implemento la clase Conjunto.cs la cual contiene 2 metodos  y ademas una collecion de COmparables debido a que es una estructura de Datos al igual que Pila.cs y Cola.cs

---
+++ Practica01/Practica02/colecciones/Conjunto.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Practica02
+{
+	/// <summary>
+	/// Description of Conjunto.
+	/// </summary>
+	public class Conjunto
+	{
+		private List<Comparable> listaElementos;
+		public Conjunto()
+		{
+			listaElementos = new List<Comparable>();
+		}
+		
+		public void agregar(Comparable c){
+			if(!pertenece(c)){
+				this.listaElementos.Add(c);
+			}
+		}
+		public bool pertenece(Comparable c){
+			foreach (Comparable e in this.listaElementos) {
+				if(c.sosIgual(e)){
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
