9981f2d5dc5a1efc0a8d76ae3877ba279bb5f7db
Huarachi Wilson <will.ehm.dev@gmail.com>
2026-04-24 20:55:27 -0300

    Ejercicio 8: Creamos la Clase Profesor el cual tiene 2 metodos

---
+++ Practica01/Practica03/Profesor.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+
+namespace Practica03
+{
+	/// <summary>
+	/// Description of Profesor.
+	/// </summary>
+	public class Profesor : Persona
+	{
+		//atributos
+		private int antiguedad;
+		//constructor
+		public Profesor(string nom,int doc,int ant):base(nom,doc)
+		{
+			this.antiguedad = ant;
+		}
+		//getter
+		public int getAntiguedad(){
+			return this.antiguedad;
+		}
+		public void hablarALaClase(){
+			Console.WriteLine("Hablando de algun tema");
+		}
+		public void escribirEnElPizarron(){
+			Console.WriteLine("Escribiendo en el pizarron");
+		}
+	}
+}
